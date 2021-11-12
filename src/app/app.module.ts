f9e6f2e8f5dd59c048af1bda99ec38f26c2ca00a
Merritt Blanks <mhblanks@gmail.com>
2021-11-11 17:18:00 -0700

    add reservation form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReservationFormComponent
+    ReservationFormComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+    MatNativeDateModule,
+    MatDatepickerModule
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationFormComponent implements OnInit {
 
-  constructor() { }
+  customerForm = this.fb.group({
+    customerName: this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+    }),
+    address: this.fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required]
+    }),
+    vehicleInfo: this.fb.group({
+      year: ['', Validators.required],
+      make: ['', Validators.required],
+      model: ['', Validators.required],
+      licensePlate: ['', Validators.required],
+      vin: ['', Validators.required],
+    }),
+    booking: this.fb.group({
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+    })
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.customerForm.valid) {
+      alert(JSON.stringify(this.customerForm.value))
+    }
   }
 
 }
